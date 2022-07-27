58024b36aaa35d86f0ebcb4434ea81befdce9758
Arjun Guha <a.guha@northeastern.edu>
2022-07-27 20:57:59 +0000

    InCoder

--- src/model_incoder.py
+++ src/model_incoder.py
@@ -17,11 +17,11 @@ model = model.half().cuda()
 
 BOS = "<|endoftext|>"
 
-def generate(input: str, max_to_generate: int=128, temperature: float=0.2):
+def incoder_completion(prompt: str, stop_tokens=[], max_to_generate: int=512, temperature: float=0.2):
     """
     Do standard left-to-right completion of the prefix `input` by sampling from the model
     """
-    input_ids = tokenizer(input, return_tensors="pt").input_ids
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
     input_ids = input_ids.cuda()
     max_length = max_to_generate + input_ids.flatten().size(0)
     if max_length > 2048:
@@ -30,24 +30,13 @@ def generate(input: str, max_to_generate: int=128, temperature: float=0.2):
         output = model.generate(input_ids=input_ids, do_sample=True, top_p=0.95, temperature=temperature, max_length=max_length)
     # pass clean_up_tokenization_spaces=False to avoid removing spaces before punctuation, e.g. "from ." -> "from."
     detok_hypo_str = tokenizer.decode(output.flatten(), clean_up_tokenization_spaces=False)
+    
     if detok_hypo_str.startswith(BOS):
         detok_hypo_str = detok_hypo_str[len(BOS):]
-    HUMAN_EVAL_STOP_WORDS = ["\nclass", "\ndef", "\n#", "\nif"]
-    for tok in HUMAN_EVAL_STOP_WORDS:
+    # Skip the prompt (which may even have stop_tokens)
+    detok_hypo_str = detok_hypo_str[len(prompt):]
+    for tok in stop_tokens:
         if tok in detok_hypo_str:
-            return detok_hypo_str.split(tok)[0]
+            detok_hypo_str = detok_hypo_str.split(tok)[0]
+            break
     return detok_hypo_str
-
-def docstring_to_code(max_to_generate=128, temperature=0.2):
-    example = '''\
-def countN(f):
-    """ Count the number of occurrences of each word in the file. """\n'''
-    result = generate(example, max_to_generate=max_to_generate, temperature=temperature)
-    print("completion:")
-    print(result)
-    return result
-
-if __name__ == "__main__":
-    print("docstring to code test:")
-    docstring_to_code()
-
