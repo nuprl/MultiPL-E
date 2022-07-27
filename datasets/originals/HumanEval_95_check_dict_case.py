9889aa8d0d8589fe0a04c08020cdbee3340bd9bd
elleven11 <federico.cassano@federico.codes>
2022-07-26 23:59:25 -0400

    type mismatch

--- datasets/originals/HumanEval_95_check_dict_case.py
+++ datasets/originals/HumanEval_95_check_dict_case.py
@@ -8,7 +8,7 @@ def check_dict_case(dict: Dict[str, str]) -> bool:
     Examples:
     check_dict_case({"a":"apple", "b":"banana"}) should return True.
     check_dict_case({"a":"apple", "A":"banana", "B":"banana"}) should return False.
-    check_dict_case({"a":"apple", 8:"banana", "a":"apple"}) should return False.
+    check_dict_case({"a":"apple", "8":"banana", "a":"apple"}) should return False.
     check_dict_case({"Name":"John", "Age":"36", "City":"Houston"}) should return False.
     check_dict_case({"STATE":"NC", "ZIP":"12345" }) should return True.
     """
@@ -42,7 +42,7 @@ def check(candidate):
     # Check some simple cases
     assert candidate({"p":"pineapple", "b":"banana"}) == True, "First test error: " + str(candidate({"p":"pineapple", "b":"banana"}))
     assert candidate({"p":"pineapple", "A":"banana", "B":"banana"}) == False, "Second test error: " + str(candidate({"p":"pineapple", "A":"banana", "B":"banana"}))
-    assert candidate({"p":"pineapple", 5:"banana", "a":"apple"}) == False, "Third test error: " + str(candidate({"p":"pineapple", 5:"banana", "a":"apple"}))
+    assert candidate({"p":"pineapple", "5":"banana", "a":"apple"}) == False, "Third test error: " + str(candidate({"p":"pineapple", "5":"banana", "a":"apple"}))
     assert candidate({"Name":"John", "Age":"36", "City":"Houston"}) == False, "Fourth test error: " + str(candidate({"Name":"John", "Age":"36", "City":"Houston"}))
     assert candidate({"STATE":"NC", "ZIP":"12345" }) == True, "Fifth test error: " + str(candidate({"STATE":"NC", "ZIP":"12345" }))      
     assert candidate({"fruit":"Orange", "taste":"Sweet" }) == True, "Fourth test error: " + str(candidate({"fruit":"Orange", "taste":"Sweet" }))      
