a6dd65a73c116daa4fb7cb3886069492d7e355e9
Arjun Guha <a.guha@northeastern.edu>
2022-07-28 09:20:50 -0400

    Parallel evaluation of Ruby YAML

--- src/problem_yaml.py
+++ src/problem_yaml.py
@@ -1,4 +1,4 @@
-from yamlize import Object, Attribute, Sequence, StrList
+from yamlize import Object, Attribute, Sequence, StrList, Typed
 
 class Problem(Object):
     name = Attribute(type=str)
@@ -7,5 +7,21 @@ class Problem(Object):
     tests = Attribute(type=str)
     completions = Attribute(type=StrList)
     stop_tokens = Attribute(type=StrList)
+
+class Result(Object):
+    program = Attribute(type=str)
+    stdout = Attribute(type=str)
+    stderr = Attribute(type=str)
+    exit_code = Attribute(type=int)
+    status = Attribute(type=str)
+
+class ResultList(Sequence):
+    item_type = Result
+
+class TestResults(Object):
+    name = Attribute(type=str)
+    language = Attribute(type=str)
+    results = Attribute(type=ResultList)
+
 
 
