10499d2de79979f1af5a9ae22d35e75dece445d6
Ming-Ho Yee <mh@mhyee.com>
2022-07-28 18:49:19 +0000

    Update Julia results to fix CI

--- datasets/jl-keep-code_davinci_001_temp_0.2-0/HumanEval_1_separate_paren_groups.jl
+++ datasets/jl-keep-code_davinci_001_temp_0.2-0/HumanEval_1_separate_paren_groups.jl
@@ -6,20 +6,33 @@
     ['()', '(())', '(()())']
     """
 function separate_paren_groups(paren_string::String)::Vector{String} 
-    paren_string = replace(paren_string, ' ', '')
-    paren_groups = Vector{String}()
-    paren_group = ""
-    for i in 1:length(paren_string)
-        if paren_string[i] == '('
-            paren_group = paren_group * paren_string[i]
-        elseif paren_string[i] == ')'
-            paren_group = paren_group * paren_string[i]
-            paren_groups = push!(paren_groups, paren_group)
-            paren_group = ""
-        end
-    end
-    return paren_groups
-end
+    paren_string = replace(paren_string, " ", "")
+    paren_string = replace(paren_string, ")(", ") (")
+    paren_string = replace(paren_string, ")", " )")
+    paren_string = replace(paren_string, "(", " (")
+    paren_string = replace(paren_string, ")", ") ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(paren_string, "  ", " ")
+    paren_string = replace(
 
 using Test
 
