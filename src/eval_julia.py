bf350f7c8e42ac3596e5b6ea366837d51e607952
John Gouwar <jgouwar@gmail.com>
2022-07-28 14:46:31 +0000

    fixed julia scripts

--- src/eval_julia.py
+++ src/eval_julia.py
@@ -1,19 +1,19 @@
 # Authored by Arjun Guha, edited by John Gouwar
 # Copyright (c) 2022, Roblox Inc.
 #
-# This script runs the Luafied HumanEval programs in datasets/julia
+# This script runs the Juliated HumanEval programs in datasets/julia
 import os
 import subprocess
 from pathlib import Path
 
 def main():
-    directory = Path(Path(__file__).parent, "..", "datasets", "rb").resolve()
+    directory = Path(Path(__file__).parent, "..", "datasets", "jl-keep-code_davinci_001_temp_0.2-0").resolve()
 
     for filename in os.listdir(directory):
         try:
             # Assumes exit-code 0 is all okay
             subprocess.check_output(" ".join(["julia", os.path.join(directory, filename)]),
-                                        stderr=subprocess.DEVNULL, shell=True, timeout=5)
+                                        stderr=None, shell=True, timeout=20)
             
             
             
@@ -24,7 +24,7 @@ def main():
             #print(str(exc.output))
             status = "Exception"
         filename = filename.split(".")[0]
-        print(f"Ruby,{filename},{status}")
+        print(f"Julia,{filename},{status}")
 
 if __name__ == "__main__":
     main()
