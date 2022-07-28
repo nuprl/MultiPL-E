f07ea9dd76593b80240ab11a9d1e4fb85460a45d
Abhinav Jangda <abhijangda@gmail.com>
2022-07-28 12:57:07 +0000

    Dataset #39 for C++

--- datasets/cpp-keep-code_davinci_001_temp_0.2/HumanEval_39_prime_fib.cpp
+++ datasets/cpp-keep-code_davinci_001_temp_0.2/HumanEval_39_prime_fib.cpp
@@ -1,8 +1,3 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<map>
-#include<optional>
 #include<assert.h>
 #include<bits/stdc++.h>
 // prime_fib returns n-th number that is a Fibonacci number and it's also prime.
@@ -16,12 +11,14 @@
 // 13
 // >>> prime_fib(5)
 // 89
- prime_fib(long n) {
-    long a = 0, b = 1, c = 0;
-    for (long i = 0; i < n; i++) {
+long prime_fib(long n) {
+    assert(n > 0);
+    long a = 0, b = 1, c = 1;
+    while (n > 0) {
         c = a + b;
         a = b;
         b = c;
+        --n;
     }
     return c;
 }
@@ -29,14 +26,14 @@
 
 int main() {
     auto candidate = prime_fib;
-    assert(candidate((1)) == ((2)));
-    assert(candidate((2)) == ((3)));
-    assert(candidate((3)) == ((5)));
-    assert(candidate((4)) == ((13)));
-    assert(candidate((5)) == ((89)));
-    assert(candidate((6)) == ((233)));
-    assert(candidate((7)) == ((1597)));
-    assert(candidate((8)) == ((28657)));
-    assert(candidate((9)) == ((514229)));
-    assert(candidate((10)) == ((433494437)));
+    assert(candidate((1)) == (2));
+    assert(candidate((2)) == (3));
+    assert(candidate((3)) == (5));
+    assert(candidate((4)) == (13));
+    assert(candidate((5)) == (89));
+    assert(candidate((6)) == (233));
+    assert(candidate((7)) == (1597));
+    assert(candidate((8)) == (28657));
+    assert(candidate((9)) == (514229));
+    assert(candidate((10)) == (433494437));
 }
