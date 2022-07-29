ed1f0304144e515fe771855878f9ba5045f9e6e2
Abhinav Jangda <abhijangda@gmail.com>
2022-07-29 20:25:00 +0000

    JavaTranslator working for most files

--- src/humaneval_to_java.py
+++ src/humaneval_to_java.py
@@ -19,7 +19,7 @@ class JavaTranslator(CPPTranslator):
         self.float_type = "float"
         self.int_type = "long"
         self.bool_type = "boolean"
-        self.none_type = "{}"
+        self.none_type = "Optional.empty()"
         self.list_type = "ArrayList<%s>"
         self.tuple_type = "Pair<%s>"
         self.dict_type = "HashMap<%s, %s>"
@@ -32,7 +32,9 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
 }"""
 
     def make_list(self, elem_type, list_contents):
-        return "new " + self.list_type%self.box_type(elem_type) + "(Arrays.asList" + list_contents + ")"
+        p = "new " + self.list_type%self.box_type(elem_type) + "(Arrays.asList" + list_contents + ")"
+        print(36, p)
+        return p
     
     def make_array_literal(self, list_contents):
         return "(" + list_contents + ")"
@@ -42,6 +44,12 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
     
     def make_map_literal(self, keys, values):
         return ", ".join(f"{k}, {v}" for k, v in zip(keys, values))
+
+    def make_optional_type(self, types):
+        return self.optional_type % self.box_type(types)
+
+    def make_optional(self, types):
+        return "Optional.of" #Add the same make_optional in C++
 
     def make_map(self, dict_type, map_literal):
         java_type = self.pytype_to_cpptype(dict_type) + "(Map"
@@ -148,8 +156,10 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
     def return_default_value(self, java_type):
         if self.is_primitive_type(java_type):
             return f"return getDefaultValue({java_type}.class);"
-        elif "Pair" in java_type:
+        elif "Pair" in java_type: #TODO: use make_optional/make_tuple to createthem and search for self.tuple_type
             return f"return new {java_type}(0l, 0l);"
+        elif "Optional" in java_type:
+            return f"return Optional.empty();"
         else:
             return f"return new {java_type}();"
 
@@ -171,11 +181,33 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
         "}\n"
         ]
     
+    def update_type(self, right: Tuple[ast.Expr, str], expected_type: Tuple[str]) -> str:
+        if self.is_primitive_type(expected_type) and self.pytype_to_cpptype(right[1]) != expected_type:
+            return f"({expected_type}){right[0]}"
+
+        return CPPTranslator.update_type(self, right, expected_type) #TODO: Use super?
+
+    def deep_equality(self, left: Tuple[str, ast.Expr], right: Tuple[str, ast.Expr]) -> str:
+        """
+        All tests are assertions that compare deep equality between left and right.
+        In C++ using == checks for structural equality
+        """
+        right = self.update_type(right, self.ret_cpp_type)
+        #Empty the union declarations
+        self.union_decls = {}
+        if self.is_primitive_type(self.ret_cpp_type):
+            return f"    assert({left[0]} == {right});"
+        else:
+            return f"    {left[0]}.equals({right});"
+
     def find_type_to_coerce(self, expr):
         '''
         '''
+        
         if "Arrays.asList(" in expr:
-            return expr[expr.index("new ")+len("new "):expr.index("Arrays.asList(")]
+            return [expr[expr.index("new ")+len("new "):expr.index("Arrays.asList(")]]
+        if 'Optional.of(' in expr:
+            return []
         return re.findall("new (.+)\(", expr)
 
     def gen_literal(self, c: bool | str | int | float | None) -> Tuple[str, ast.Name]:
