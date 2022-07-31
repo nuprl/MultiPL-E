79fe55420c6f694b017aa3b4968a644395433b42
Abhinav Jangda <abhijangda@gmail.com>
2022-07-31 04:31:54 +0000

    Refactoring common C# and Java Translator methods in C++

--- src/humaneval_to_java.py
+++ src/humaneval_to_java.py
@@ -13,52 +13,70 @@ JAVA_CLASS_NAME = "Problem"
 #Refactoring needed
 
 class JavaTranslator(CPPTranslator):
-    stop = ["    }\n    //","    }\n    p", "    }\n}","    }\n\n"]
+    stop = ["    }\n    //","    }\n    p", "    }\n}","\n    }\n"]
 
     def __init__(self, file_ext):
         super().__init__(file_ext)
         self.string_type = "String"
         self.float_type = "float"
-        self.int_type = "long"
+        self.int_type = "int"
         self.bool_type = "boolean"
         self.none_type = "Optional.empty()"
         self.list_type = "ArrayList<%s>"
-        self.tuple_type = "Pair<%s>"
-        self.dict_type = "HashMap<%s, %s>"
-        self.optional_type = "Optional<%s>"
+        self.tuple_type = "Pair"
+        self.dict_type = "HashMap"
+        self.optional_type = "Optional"
         self.any_type = "std::any"
         self.indent = "    "
-        self.get_default_val_def = """private static <T> T getDefaultValue(Class<T> clazz) {
-return (T) Array.get(Array.newInstance(clazz, 1), 0);
-}"""
+        self.make_tuple = "Pair.with"
+        self.make_optional = "Optional.of"
 
-    def make_list(self, elem_type, list_contents):
+    def gen_list_type(self, elem_type):
+        print(elem_type, self.box_type(elem_type))
+        return self.list_type % self.box_type(elem_type)
+
+    def gen_make_list(self, elem_type, list_contents):
         if list_contents == "":
             list_contents = "()"
         return "new " + self.list_type%self.box_type(elem_type) + "(Arrays.asList" + list_contents + ")"
     
-    def make_array_literal(self, list_contents):
+    def gen_dict_type(self, ktype, vtype):
+        return self.dict_type + "<%s,%s>"  % (self.box_type(ktype), self.box_type(vtype))
+
+    def gen_array_literal(self, list_contents):
         return "(" + list_contents + ")"
     
-    def make_tuple(self, elem_types):
-        return "Pair.with"
-    
-    def make_map_literal(self, keys, values):
+    def gen_map_literal(self, keys, values):
         return ", ".join(f"{k}, {v}" for k, v in zip(keys, values))
 
-    def make_optional_type(self, types):
-        return self.optional_type % self.box_type(types)
+    def gen_optional_type(self, types):
+        return self.optional_type + "<%s>" % self.box_type(types)
 
-    def make_optional(self, types):
-        return "Optional.of" #Add the same make_optional in C++
+    def gen_optional(self, types, elem):
+        return f"{self.make_optional}(%s)"%elem
 
-    def make_map(self, dict_type, map_literal):
-        java_type = self.pytype_to_cpptype(dict_type) + "(Map"
+    def gen_map(self, dict_type, map_literal):
+        java_type = self.translate_pytype(dict_type) + "(Map"
         return f"new {java_type}.of({map_literal}))"
+
+    def gen_tuple_type(self, elem_types):
+        return self.tuple_type + "<%s>" % ", ".join([self.box_type(et) for et in elem_types])
 
     def box_type(self, primitive_type):
         if self.is_primitive_type(primitive_type):
-            return primitive_type.capitalize()
+            match primitive_type:
+                case "int":
+                    return "Integer"
+                case "float":
+                    return "Float"
+                case "long":
+                    return "Long"
+                case "boolean":
+                    return "Boolean"
+                case "double":
+                    return "Double"
+                case _other:
+                    raise Exception("Unknown primitive type '%s'"%primitive_type)
         
         return primitive_type
 
@@ -72,65 +90,6 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
             "import java.io.*;",
             "import java.util.stream.*;"
         ]) + "\n"
-
-    def pytype_to_cpptype(self, ann: ast.expr | None) -> str:
-        '''Traverses an AST annotation and generates Java Types
-            Could have just used CPPTranslator's method but Java wants boxed types
-        '''
-
-        if ann == None:
-            raise Exception(f"No annotation")
-
-        match ann:
-            case ast.Name(id="str"):
-                return self.string_type
-            case ast.Name(id="int"):
-                return self.int_type
-            case ast.Name(id="float"):
-                return self.float_type
-            case ast.Name(id="bool"):
-                return self.bool_type
-            case ast.Name(id="None"):
-                #It appears None is always used in optional
-                return self.none_type
-            case ast.List(elts=elts):
-                return self.list_type % self.box_type(self.pytype_to_cpptype(elts[0]))
-            case ast.Tuple(elts=elts):
-                return self.tuple_type % ", ".join([self.box_type(self.pytype_to_cpptype(e)) for e in elts])
-            case ast.Dict(keys=k,values=v):
-                return self.dict_type % (self.box_type(self.pytype_to_cpptype(k)), self.box_type(self.pytype_to_cpptype(v)))
-            case ast.Subscript(value=ast.Name(id="Dict"), slice=ast.Tuple(elts=key_val_type)):
-                return self.dict_type % (self.box_type(self.pytype_to_cpptype(key_val_type[0])), self.box_type(self.pytype_to_cpptype(key_val_type[1])))
-            case ast.Subscript(value=ast.Name(id="List"), slice=elem_type):
-                return self.list_type % self.box_type(self.pytype_to_cpptype(elem_type))
-            case ast.Subscript(value=ast.Name(id="Tuple"), slice=elts):
-                if type(elts) is ast.Tuple:
-                    return self.box_type(self.pytype_to_cpptype(elts))
-                return self.list_type % self.box_type(self.pytype_to_cpptype(elts))
-            case ast.Subscript(value=ast.Name(id="Optional"), slice=elem_type):
-                return self.optional_type % self.box_type(self.pytype_to_cpptype(elem_type)) 
-            case ast.Subscript(value=ast.Name(id="Union"), slice=ast.Tuple(elts=elems)):
-                union_elems_types = []
-                union_decl = {}
-                for i,e in enumerate(elems):
-                    elem_type = self.pytype_to_cpptype(e)
-                    union_elems_types += [elem_type]
-                    union_decl[elem_type] = f"f{i}" 
-
-                union_name = ("Union_%s"%("_".join(union_elems_types))).replace("::", "_").replace("<", "_").replace(">", "_")
-                if union_name not in self.union_decls:
-                    self.union_decls[union_name] = union_decl
-
-                return union_name
-            case ast.Name(id="Any"):
-                return self.any_type;
-            case ast.Constant(value=None):
-                return self.none_type
-            case ast.Constant(value=Ellipsis):
-                return ""
-            case _other:
-                print(f"Unhandled annotation: {ast.dump(ann)}")
-                raise Exception(f"Unhandled annotation: {ann}")
     
     def translate_prompt(self, name: str, args: List[ast.arg], _returns, description: str) -> str:
         '''Translate Python prompt to Java.
@@ -144,50 +103,51 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
             return components[0] + ''.join(x.title() for x in components[1:])
 
         class_decl = f"class {JAVA_CLASS_NAME} {{\n"
-#        class_decl += self.get_default_val_def
         indent = "    "
         comment_start = self.indent + "//"
         java_description = (
             comment_start +" " + re.sub(DOCSTRING_LINESTART_RE, "\n" + comment_start + " ", description.strip()) + "\n"
         )
-        self.args_type = [self.pytype_to_cpptype(arg.annotation) for arg in args]
-        formal_args = [f"{self.pytype_to_cpptype(arg.annotation)} {self.gen_var(arg.arg)[0]}" for arg in args]
+        self.args_type = [self.translate_pytype(arg.annotation) for arg in args]
+        formal_args = [f"{self.translate_pytype(arg.annotation)} {self.gen_var(arg.arg)[0]}" for arg in args]
         formal_arg_list = ", ".join(formal_args)
         #Transform entry point to Java style Camel case
         self.entry_point = to_camel_case(name)
         self.ret_ann = _returns
-        self.ret_cpp_type = self.pytype_to_cpptype(_returns)
-        java_prompt = f"{self.module_imports()}{class_decl}{java_description}{self.indent}public static {self.ret_cpp_type} {self.entry_point}({formal_arg_list})" + " {\n"
+        self.translated_return_type = self.translate_pytype(_returns)
+        java_prompt = f"{self.module_imports()}{class_decl}{java_description}{self.indent}public static {self.translated_return_type} {self.entry_point}({formal_arg_list})" + " {\n"
 
         return java_prompt
     
     def is_primitive_type(self, java_type):
         return java_type in [self.float_type, self.bool_type, self.int_type]
 
-    def return_default_value(self, java_type):
-        if self.is_primitive_type(java_type):
-            return f"return getDefaultValue({java_type}.class);"
-        elif "Pair" in java_type: #TODO: use make_optional/make_tuple to createthem and search for self.tuple_type
-            if "Long, Long" in java_type:
-                return f"return new {java_type}(0l, 0l);"
-            elif "Float, Float" in java_type:
-                return f"return new {java_type}(0f, 0f);"
-            elif "Boolean, Boolean" in java_type:
-                return f"return new {java_type}(true, true);"
-            elif "String, String" in java_type:
-                return f"return new {java_type}(\"\", \"\");"
-            elif "String, Boolean" in java_type:
-                return f"return new {java_type}(\"\", false);"
-            elif "Optional<Long>, Optional<Long>" in java_type:
-                return f"return new {java_type}(Optional.of(1l), Optional.of(1l));"
-            elif "String, " in java_type:
-                return f"return new {java_type}(\"\", \"\");"
-            else:
-                assert False, "Unknown " + java_type
-        elif "Optional" in java_type:
-            return f"return Optional.empty();"
+    def is_boxed_type(self, boxed_type):
+        return boxed_type in [self.box_type(t) for t in [self.float_type, self.bool_type, self.int_type]]
+
+    def return_default_value(self, csharp_type): #make this function name default value and add it in C++ Translator 
+        if self.is_primitive_type(csharp_type) or self.is_boxed_type(csharp_type):
+            if self.int_type in csharp_type or self.box_type(self.int_type) in csharp_type:
+                return "0"
+            elif self.float_type in csharp_type or self.box_type(self.float_type) in csharp_type:
+                return "0.0f"
+            elif self.bool_type in csharp_type or self.box_type(self.bool_type) in csharp_type:
+                return "true"
+        elif self.string_type == csharp_type:
+            return '""'
+        elif csharp_type.find("List<") == 0:
+            elem_type = re.findall(rf'List<(.+)>', csharp_type)[0]
+            #List default is: new List<T>()
+            return self.gen_make_list(elem_type, "")
+        elif csharp_type.find(f"{self.tuple_type}<") == 0 : #TODO: use gen_optional/gen_make_list to createthem and search for self.tuple_type
+            template_types = re.findall(rf'{self.tuple_type}<(.+),(.+)>', csharp_type)[0]
+            first_default = self.return_default_value(template_types[0].strip())
+            second_default = self.return_default_value(template_types[1].strip())
+            return self.gen_make_tuple(first_default + "," + second_default)
+        elif csharp_type.find(self.optional_type) == 0:
+            return f"({csharp_type})null"
         else:
-            return f"return new {java_type}();"
+            return f"new {csharp_type}()"
 
     def test_suite_prefix_lines(self, entry_point) -> List[str]:
         """
@@ -195,7 +155,7 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
         """
 
         return [
-            # self.return_default_value(self.ret_cpp_type),
+            "return " + self.return_default_value(self.translated_return_type) + ";",
             self.indent + "}",
             self.indent + "public static void main(String[] args) {",
         ]
@@ -208,7 +168,7 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
         ]
     
     def update_type(self, right: Tuple[ast.Expr, str], expected_type: Tuple[str]) -> str:
-        if self.is_primitive_type(expected_type) and self.pytype_to_cpptype(right[1]) != expected_type:
+        if self.is_primitive_type(expected_type) and self.translate_pytype(right[1]) != expected_type:
             return f"({expected_type}){right[0]}"
 
         return CPPTranslator.update_type(self, right, expected_type) #TODO: Use super?
@@ -218,10 +178,10 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
         All tests are assertions that compare deep equality between left and right.
         In C++ using == checks for structural equality
         """
-        right = self.update_type(right, self.ret_cpp_type)
+        right = self.update_type(right, self.translated_return_type)
         #Empty the union declarations
         self.union_decls = {}
-        if self.is_primitive_type(self.ret_cpp_type):
+        if self.is_primitive_type(self.translated_return_type):
             return f"    assert({left[0]} == {right});"
         else:
             return f"    assert({left[0]}.equals({right}));"
@@ -243,7 +203,7 @@ return (T) Array.get(Array.newInstance(clazz, 1), 0);
         if type(c) == float:
             return repr(c) + "f", ast.Name(id="float")
         if type(c) == int:
-            return repr(c) + "l", ast.Name(id="int")
+            return repr(c), ast.Name(id="int")
         return CPPTranslator.gen_literal(self, c)
 
     def gen_call(self, func: str, args: List[Tuple[str, ast.Expr]]) -> Tuple[str, None]:
