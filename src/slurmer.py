bafa999336761f9c328f8af1bc7a8e8d5fcdea25
Luna Phipps-Costin <phipps-costin.l@northeastern.edu>
2022-08-02 13:30:25 -0400

    Add rust results

--- src/slurmer.py
+++ src/slurmer.py
@@ -4,8 +4,8 @@ import os
 user = os.getenv("USER")
 if user == "a.guha":
   SUPPORTED = [ "php", "/rkt-" ]
-elif user == "l.phipps-coston":
-  SUPPORTED = [ ]
+elif user == "l.phipps-costin":
+  SUPPORTED = [ "rust", "/rs-" ]
 elif user == "zi.ya":
   SUPPORTED = [ "lua" ]
 else:
@@ -16,15 +16,18 @@ files = [ ]
 for p in Path("../experiments").glob("*/*.yaml"):
     if p.name.endswith(".results.yaml"):
         continue
+    if "-remove" in str(p):
+        continue
     if p.with_suffix(".results.yaml").exists():
         continue
     p_str = str(p)
     if not any([ lang in p_str for lang in SUPPORTED ]):
         continue
-    if p.stat().st_size < (1024 * 2):
+    if p.stat().st_size < (1024 * 8):
         continue
     count += 1
     files.append(p_str)
+print(f"wrote {count} upcoming yaml paths to files.txt")
 
 with open("files.txt", "w") as f:
     for p in files:
