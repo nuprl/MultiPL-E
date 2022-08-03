15c948cdc950de9fc0fdcbc50565a263b6e3de2a
canders1 <canders277@gmail.com>
2022-08-03 16:46:43 -0400

    checkcompletions

--- src/check_completeness.py
+++ src/check_completeness.py
@@ -26,23 +26,29 @@ def check(lang, model, temp, docstrings):
 
     dir = Path("../experiments") / (lang + "-" + model + "-" + temp + "-" + docstrings)
     if not dir.exists():
-        print(f"{lang},{docstrings},{model},{temp}")
+        print(f"{lang},{docstrings},{model},{temp} does not exist")
         return
 
-    problems = [ p for p in dir.glob("*.yaml") if not p.name.endswith(".results.yaml") ]
-    if len(problems) != 161:
-        print(f"{dir} has {len(problems)} problems")
-        #return
+    all_problems = [ p for p in dir.glob("*.yaml")]
+    problems = [p for p in all_problems if not p.name.endswith(".results.yaml") ]
+    tested_problems = [p for p in all_problems if p.name.endswith(".results.yaml")]
+    pcount = len(problems)
+    tcount = len(tested_problems)
     
-    #print(len(problems))
     for p in problems:
         problem = yaml.load(open(p), yaml.Loader)
-        #print(problem['name'])
-        # if problem['name'] == 'HumanEval_94_skjkasdkd':
-        #     print(len(problem['completions']))
-        if len(problem['completions']) != 200 and len(problem['completions']) > 0:
+        if len(problem['completions']) != 200:
             print(f"{problem['name']} in {dir} has only {len(problem['completions'])} completions")
-            #return
+            pcount -= 1
+
+    for p in tested_problems:
+        problem = yaml.load(open(p), yaml.Loader)
+        if len(problem['results']) != 200:
+            print(f"{problem['name']} in {dir} has only {len(problem['results'])} results")
+            tcount -= 1
+
+    print(f"{lang},{docstrings},{model},{temp}: has {pcount} fully generated problems.")
+    print(f"{lang},{docstrings},{model},{temp}: has {tcount} fully tested results.")
 
 def check_all():
     #with ThreadPoolExecutor() as executor:
