2ebf454e0a9b96bf241e7fe7935a359ba282d8c6
Abhinav Jangda <abhijangda@gmail.com>
2022-08-03 02:05:03 +0000

    Support 2 more benchmarks in C# Translator

--- datasets/cs-keep-code_davinci_001_temp_0.2/HumanEval_22_filter_integers.cs
+++ datasets/cs-keep-code_davinci_001_temp_0.2/HumanEval_22_filter_integers.cs
@@ -2,21 +2,29 @@ using System;
 using System.Numerics;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
 class Problem {
     // Filter given list of any python values only for integers
     // >>> filter_integers(['a', 3.14, 5])
     // [5]
     // >>> filter_integers([1, 2, 3, 'abc', {}, []])
     // [1, 2, 3]
-    public static List<long> FilterIntegers(List<std::any> values) {
+    public static List<int> FilterIntegers(List<object> values) {
+        List<int> result = new List<int>();
+        foreach (object value in values) {
+            if (value is int) {
+                result.Add((int)value);
+            }
+        }
+        return result;
 
-
-return new List<long>();
     }
     public static void Main(string[] args) {
-    Debug.Assert(FilterIntegers((new List<std::any>())).Equals((new List<long>())));
-    Debug.Assert(FilterIntegers((new List<long>(new long[]{(long)4L, (long)new List<std::any>(), (long)23.2f, (long)9L, (long)"adasd"}))).Equals((new List<long>(new long[]{(long)4L, (long)9L}))));
-    Debug.Assert(FilterIntegers((new List<std::any>(new long[]{(long)3L, (long)"c", (long)3L, (long)3L, (long)"a", (long)"b"}))).Equals((new List<long>(new long[]{(long)3L, (long)3L, (long)3L}))));
+    Debug.Assert(FilterIntegers((new List<object>())).Equals((new List<int>())));
+    Debug.Assert(FilterIntegers((new List<object>(new object[]{4, new List<object>(), 23.2f, 9, "adasd"}))).Equals((new List<int>(new int[]{(int)4, (int)9}))));
+    Debug.Assert(FilterIntegers((new List<object>(new object[]{3, "c", 3, 3, "a", "b"}))).Equals((new List<int>(new int[]{(int)3, (int)3, (int)3}))));
     }
 
 }
