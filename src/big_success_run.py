7e7c4dcd25b24cee8be662b049ac927cfefeb210
Molly Q Feldman <mfeldman@oberlin.edu>
2022-08-04 12:24:01 -0400

    setup complete for pass at k prelims

--- src/big_success_run.py
+++ src/big_success_run.py
@@ -12,14 +12,15 @@ from csv import reader
 
 #TODO: do for all languages or necessary subset - right now this is a good test set 
 
-LANG = ['cpp']#,'d']#[ "py", "sh","cs","cpp","d","go","java","js", "jl", "lua", "pl", "php", "r", "rkt", "rb", "rs","scala","swift","ts",]
+LANG = ['rkt']#,'d']#[ "py", "sh","cs","cpp","d","go","java","js", "jl", "lua", "pl", "php", "r", "rkt", "rb", "rs","scala","swift","ts",]
 MODEL = [ "incoder", "davinci" ]
-TEMP = [ "0.8", "0.2", ]
-DOCSTRINGS = [ "keep"]# "remove", "reworded" ] #original keep 
-TERMS = ['keep']#[ "keep", "transform" ]
+TEMP = [ "0.2"] # come back to this
+
+ENDINGMAP = {'reworded': ['transform', 'transform'], 'keep': ['keep', 'keep'], \
+                'remove': ['remove', 'keep'], 'transform': ['transform', 'keep']}
 
 def checkPassResults(lang,model,temp,docstrings,term):
-    passOne = Path('../model_results/all-pass-at-1.csv')
+    passOne = Path('../model_results/all-pass-at-1-eval-run.csv')
     with open(passOne) as csvfile:
                 values = reader(csvfile)
                 for v in values:
@@ -32,21 +33,21 @@ def main():
     for lang in LANG:
             for model in MODEL:
                 for temp in TEMP:
-                    for docstrings in DOCSTRINGS:
-                        for term in TERMS:
-                            print()
-                            findResults = Path('../experiments/'+lang+"-"+model+"-"+temp+"-"+docstrings+"/")
-                            #if there is already an entry, stop and continue! 
-                            exists = checkPassResults(lang,model,temp,docstrings,term)
-                            if exists:
-                                print(f'results exist for {findResults} already in all-pass-at-1 - move on.')
-                                continue 
-                            #TODO: add term here when file names are complete
-                            print(f'making summary for{findResults}')
-                            result = makeSummary(findResults)
-                            if result: #we succeeded in a summary, either existing or created now
-                                summary = Path('../model_results/'+lang+"-"+model+"-"+temp+'-'+docstrings+'-'+term+"-summary.csv")
-                                print(f'adding results to pass at k for {summary}')
-                                evaluate_functional_correctness(summary)         
+                    for ending in ENDINGMAP.keys():
+                        findResults = Path('../experiments/'+lang+"-"+model+"-"+temp+"-"+ending+"/")
+                        print()
+                        doc, term = ENDINGMAP[ending][0], ENDINGMAP[ending][1]
+                        #if there is already an entry, stop and continue! 
+                        exists = checkPassResults(lang,model,temp,doc,term)
+                        if exists:
+                            print(f'results exist for {findResults} already in all-pass-at-1 - move on.')
+                            continue 
+                        #TODO: add term here when file names are complete
+                        print(f'making summary for{findResults}')
+                        result = makeSummary(findResults,lang,model,temp,doc,term)
+                        if result: #we succeeded in a summary, either existing or created now
+                            summary = Path('../model_results/'+lang+"-"+model+"-"+temp+'-'+doc+'-'+term+"-summary.csv")
+                            print(f'adding results to pass at k for {summary}')
+                            evaluate_functional_correctness(summary)         
 if __name__ == "__main__":                  
     main()
