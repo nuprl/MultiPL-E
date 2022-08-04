35d6228eb77db34a3dd085d8023164b7c8d03f0b
Yangtian Zi <zi.ya@northeastern.edu>
2022-08-04 16:30:43 -0400

    more benchmarks

--- src/containerized_eval.py
+++ src/containerized_eval.py
@@ -21,6 +21,8 @@ import eval_swift
 import eval_cpp
 import eval_php
 import eval_dlang
+import eval_julia
+import eval_r
 import tempfile
 import json
 
@@ -41,7 +43,10 @@ EVALUATORS = {
     "cpp": (eval_cpp.eval_script, ".cpp"),
     "php": (eval_php.eval_script, ".php"),
     "humaneval_to_dlang.py": (eval_dlang.eval_script, ".d"),
-    "d": (eval_dlang.eval_script, ".d")
+    "d": (eval_dlang.eval_script, ".d"),
+    "r": (eval_r.eval_script, ".r"),
+    "humaneval_to_r.py": (eval_r.eval_script, ".r"),
+    "jl": (eval_julia.eval_script, ".jl")
 }
 
 def eval_script(problem, index):
