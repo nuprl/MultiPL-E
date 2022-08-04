bf27159e008b200ded7b83fdbfa2b30eff8bfede
canders1 <canders277@gmail.com>
2022-08-04 17:06:03 -0400

    fixing r

--- src/humaneval_to_r.py
+++ src/humaneval_to_r.py
@@ -10,19 +10,60 @@ from generic_translator import main
 
 DOCSTRING_LINESTART_RE = re.compile("""\n(\s+)""")
 
+needs_hashmap = False
+
+def translate_type(t):
+    global needs_hashmap
+    match t:
+        case ast.Subscript(ast.Name(id), slice, ctx):
+            match id:
+                case "List":
+                    return "list"
+                case "Union":
+                    return "union"
+                case "Tuple":
+                    return "tuple"
+                case "Dict":
+                    return "dict"
+                case "Optional":
+                    return "undefined"
+                case other:
+                    raise Exception(f"Bad generic {other}")
+        case ast.Name("int") | "int":
+            return "number"
+        case ast.Name("float"):
+            return "number"
+        case ast.Name("bool"):
+            return "boolean"
+        case ast.Name("str") | "str":
+            return "string"
+        case None:
+            return "no type"
+        case ast.Name("Any"):
+            return "any"
+        case ast.Name(x):
+            raise Exception(f"unknown name {x}")
+        case ast.Constant(Ellipsis):
+            return "ellipsis"
+        case _other:
+            raise Exception(f"unknown annotation: {t}")
+
 class Translator:
     '''R Translator
     '''
 
     stop = [ '\n#', '\n```']
     
-    def file_ext(self):
-        return "r"
+    def __init__(self):
+        self.file_ext = 'r'
 
-    def translate_prompt(self, name: str, args: List[ast.arg], _returns, description: str) -> str:
+    def translate_prompt(self, name: str, args: List[ast.arg], returns, description: str) -> str:
+        global needs_hashmap
         r_description = (
             "# " + re.sub(DOCSTRING_LINESTART_RE, "\n# ", description.strip()) + "\n"
         )
+        needs_hashmap = False
+        self.type = [[arg.annotation for arg in args], returns]
         arg_names = [arg.arg for arg in args]
         arg_list = ", ".join(arg_names)
         return f"{r_description}{name} <- function({arg_list})" + ' {'
@@ -68,7 +109,9 @@ class Translator:
         '''Translate a list with elements l
            A list [ x, y, z ] translates to list(x, y, z)
         '''
-        if len(set([type(i) for i in l])) <= 1:
+        types = [self.type[0][i] for i, arg in enumerate(l)]
+        print(types)
+        if len(set(types)) <= 1:
             return "c(" + ", ".join(l) + ")"
         return "list(" + ", ".join(l) + ")"
     
@@ -77,7 +120,9 @@ class Translator:
         '''Translate a tuple with elements t
            A tuple (x, y, z) translates to list(x, y, z) }
         '''
-        if len(set([type(i) for i in t])) <= 1:
+        types = [self.type[0][i] for i, arg in enumerate(t)]
+        print(types)
+        if len(set(types)) <= 1:
             return "c(" + ", ".join(t) + ")"
         return "list(" + ", ".join(t) + ")"
     
@@ -94,5 +139,5 @@ class Translator:
         return func + "(" + ", ".join(args) + ")"
 
 if __name__ == "__main__":
-    translator = Translator()
+    translator = RTranslator("R")
     main(translator)
