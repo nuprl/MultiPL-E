23a085cc7a7ece0dfdfa14b87cf527134cd104cc
Arjun Guha <a.guha@northeastern.edu>
2022-08-06 17:11:41 -0400

    Slurming

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # polyglot-codegen-evaluation
+
+
+
+Language   | Extension | Requirements
+-----------+-----------+----------
+Python     | py        | Conda
+JavaScript | js        | Conda
+TypeScript | ts        | Conda
+Java       | java      | Conda
+D          | d         | Conda
+C++        | cpp       | System (we have g++ 9.2.0)
+R          | r         | Manually install
+Julia      | jl        | Conda
+Bash       | sh        | System (we have bash 4.2)
+C#         | cs        | Conda
+Go         | go        | ??
+Lua        | lua       | Conda
+Perl       | pl        | Conda and `cpan install Test::Deep`
+php        | php       | Conda
+Ruby       | rb        | Conda and `gem install test-unit`
+Scala      | scala     | Manual install
+Swift      | swift     | Manual install (We use Swift 5.8)
+Racket     | rkt       | Manual install (We use Racket 8.2)
+
+Some notes:
+
+- Manually install Scala in the Conda environment, which includes
+  its own JDK.
+
--- src/eval_pl.py
+++ src/eval_pl.py
@@ -1,29 +1,13 @@
-# Authored by Arjun Guha
-import os
-import subprocess
 from pathlib import Path
+import libeval
 
-def main():
-    directory = Path(Path(__file__).parent, "..", "datasets", "pl-keep-code_davinci_001_temp_0.2").resolve()
+def eval_script(path: Path):
+    result = libeval.run_without_exn(["perl", path])
+    if "ERROR" in result["stdout"] or "ERROR" in result["stderr"] \
+        or "FAILURE" in result["stdout"] or "ERROR" in result["stderr"]:
+        result["status"] = "Exception"
+    return result
 
-    for filename in os.listdir(directory):
-
-        try:
-            # Assumes exit-code 0 is all okay
-            # Run racket on the file, capturing stderr
-            output = subprocess.check_output(["perl", os.path.join(directory, filename)],
-                                        stderr=subprocess.STDOUT,
-                                        encoding="utf-8", timeout=5)
-            if output.find('ERROR') != -1 or output.find('FAILURE') != -1:
-                status = "Exception"
-            else:
-                status = "OK"
-        except subprocess.TimeoutExpired as exc:
-            status = "Timeout"
-        except subprocess.CalledProcessError as exc:
-                status = "Exception"
-        filename = filename.split(".")[0]
-        print(f"Perl,{filename},{status}")
 
 if __name__ == "__main__":
-    main()
+    libeval.testing_mail(eval_script, "Perl", ".pl")
