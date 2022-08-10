31b0926860a80f233f891e708dce97273e340fc6
Arjun Guha <a.guha@northeastern.edu>
2022-08-10 18:30:00 +0000

    Some docs

--- src/single_experiment_pass_k.py
+++ src/single_experiment_pass_k.py
@@ -22,6 +22,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=str,  help="directory with results")
     args = parser.parse_args()
+    print("NOTE: Use pass@1 for temperature 0.2 and pass@10 or pass@100 for temperature 0.8")
+    print("[pass@1 pass@10 pass@100]")
     return np.array([ for_file(p) for p in Path(args.dir).glob("*.results.yaml") ]).mean(axis=0)
 
 if __name__ == "__main__":
