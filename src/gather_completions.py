05f22a78e4fa98ab1bfed4a548511f7da5d26d20
Arjun Guha <a.guha@northeastern.edu>
2022-08-11 10:41:33 +0000

    Refactoring

--- src/gather_completions.py
+++ src/gather_completions.py
@@ -31,21 +31,28 @@ from problem_yaml import Problem
 import argparse
 import sys
 from pathlib import Path
-from tqdm import tqdm
+import logging
+
 
 async def codex_completion(
     model: str,
-    prompt: str, stop_tokens, max_to_generate: int, temperature: float, n: int
+    prompt: str,
+    stop_tokens,
+    max_to_generate: int,
+    temperature: float,
+    n: int,
 ):
-        response = await completions.completion(
-            model=model,
-            prompt=prompt,
-            max_tokens=max_to_generate,
-            temperature=temperature,
-            n=n,
-            top_p=0.95,
-            stop=[ s for s in stop_tokens])
-        return  response
+    response = await completions.completion(
+        model=model,
+        prompt=prompt,
+        max_tokens=max_to_generate,
+        temperature=temperature,
+        n=n,
+        top_p=0.95,
+        stop=[s for s in stop_tokens],
+    )
+    return response
+
 
 async def process_problem_yaml(problem_yaml_path, args, max_to_generate):
     with problem_yaml_path.open() as f:
@@ -72,16 +79,30 @@ async def process_problem_yaml(problem_yaml_path, args, max_to_generate):
             f.write(Problem.dump(problem))
         num_completions_required -= num_samples
 
-async def parameterized_main(max_to_generate: int):
+
+def configure_logging(args):
+    format_str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    if args.log_file:
+        logging.basicConfig(
+            filename=args.log_file, level=args.log_level, format=format_str
+        )
+    else:
+        logging.basicConfig(level=args.log_level, format=format_str)
+
+
+async def main():
+    global completions
+
     args = argparse.ArgumentParser()
     args.add_argument(
         "--dir", type=str, required=True, help="Directory with problem YAMLs"
     )
-    args.add_argument(
-        "--temperature", type=float, required=True)
+    args.add_argument("--temperature", type=float, required=True)
     args.add_argument("--max-samples", type=int, required=True)
     args.add_argument("--model", type=str, required=True)
     args.add_argument("--limit-completions", type=int, default=200)
+    args.add_argument("--log-file", type=str, default=None)
+    args.add_argument("--log-level", type=str, default="INFO")
     args = args.parse_args()
 
     dir = Path(args.dir)
@@ -92,25 +113,32 @@ async def parameterized_main(max_to_generate: int):
     if args.model == "davinci":
         args.model = "code-davinci-002"
 
-    problems = list(filter(lambda f: not f.name.endswith(".results.yaml"), sorted(dir.glob("*.yaml"))))
-
-    problem_completions = (process_problem_yaml(problem_yaml_path, args, max_to_generate) for problem_yaml_path in problems)
-    await asyncio.gather(*problem_completions)
-
-
-async def main():
-    global completions
+    configure_logging(args)
 
     # Load the model keys from the CSV file.
     with open("model_keys.csv") as f:
         reader = csv.DictReader(f)
         rows = list(reader)
     model_keys = [row["Key"] for row in rows if not row["Key"].startswith("http://")]
-    other_models = [ (row["Model"], row["Key"]) for row in rows if row["Key"].startswith("http://")]
-    
-    completions = openai_multimodel_multikey.MultiModelMultiKeyCompletion(model_keys, other_models)
-    
-    await parameterized_main(max_to_generate=512)
+    other_models = [
+        (row["Model"], row["Key"]) for row in rows if row["Key"].startswith("http://")
+    ]
+
+    async with openai_multimodel_multikey.MultiModelMultiKeyCompletion(
+        model_keys, other_models
+    ) as completions:
+        problems = list(
+            filter(
+                lambda f: not f.name.endswith(".results.yaml"),
+                sorted(dir.glob("*.yaml")),
+            )
+        )
+
+        problem_completions = (
+            process_problem_yaml(problem_yaml_path, args, max_to_generate=512)
+            for problem_yaml_path in problems
+        )
+        await asyncio.gather(*problem_completions)
 
 
 if __name__ == "__main__":
