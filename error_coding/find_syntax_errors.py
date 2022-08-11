400d828f904e9f7eff2666064101b8bbbdcb7804
Donald Pinckney <donald_pinckney@icloud.com>
2022-08-11 13:24:22 -0400

    Added my swift script back in, don't know why abhinav deleted it earlier.

--- error_coding/find_syntax_errors.py
+++ error_coding/find_syntax_errors.py
@@ -15,132 +15,14 @@ except ImportError:
     from yaml import Loader as YAML_Loader
 
 
-def f_and(*funcs):
-    def the_and(*args, **kwargs):
-        return all(f(*args, **kwargs) for f in funcs)
-    return the_and
+def dummy_category(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
+    return True
 
-def f_or(*funcs):
-    def the_or(*args, **kwargs):
-        return any(f(*args, **kwargs) for f in funcs)
-    return the_or
-
-def f_not(f):
-    def the_not(*args, **kwargs):
-        return not f(*args, **kwargs)
-    return the_not
-
-def ok_category(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return exit_code == 0 and status == 'OK'
-
-def syntax_error_category(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return exit_code != 0 and status == 'SyntaxError'
-
-def exception_category(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return exit_code != 0 and status == 'Exception'
-
-def assertion_fail(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Assertion failed\nCurrent stack trace:" in stderr
-
-def unwrap_nil(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Unexpectedly found nil while unwrapping an Optional value\nCurrent stack trace:" in stderr
-
-def index_out_of_range(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Index out of range\nCurrent stack trace:" in stderr
-
-def string_index_out_of_bounds(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: String index is out of bounds\nCurrent stack trace:" in stderr
-
-def array_index_out_of_range(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Array index is out of range\nCurrent stack trace:" in stderr
-
-def invalid_range_creation(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Range requires lowerBound <= upperBound\nCurrent stack trace:" in stderr
-
-def cant_remove_last_elem_from_empty_collection(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Can't remove last element from an empty collection\nCurrent stack trace:" in stderr
-
-def cant_remove_first_elem_from_empty_collection(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Can't remove first element from an empty collection\nCurrent stack trace:" in stderr
-
-def div_by_zero_remainder(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Division by zero in remainder operation\nCurrent stack trace:" in stderr
-
-def negative_array_index(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return ": Fatal error: Negative Array index is out of range\nCurrent stack trace:" in stderr
-
-def over_under_flow(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return stderr == "" and stdout == "" and status == 'Exception' and exit_code == -4
-
-def timeout_category(exit_code: int, status: str, stderr: str, stdout: str) -> bool:
-    return exit_code != 0 and status == 'Timeout'
 
 CATEGORY_DEFINITIONS: OrderedDict[str, Tuple[str, Callable[[int, str, str, str], bool]]] = OrderedDict([
-    ('OK', ('OK', 
-        ok_category
-    )),
-    ('SyntaxError', ('Compilation error', 
-        syntax_error_category
-    )),
-    ('Exception-AssertionFail', ('An assertion failed', 
-        f_and(exception_category, assertion_fail)
-    )),
-    ('Exception-UnwrapNil', ('Nil was unwrapped', 
-        f_and(exception_category, unwrap_nil)
-    )),
-    ('Exception-IndexOutOfRange', ('Index out of range', 
-        f_and(exception_category, index_out_of_range)
-    )),
-    ('Exception-StringIndexOutOfBounds', ('String index out of bounds', 
-        f_and(exception_category, string_index_out_of_bounds)
-    )),
-    ('Exception-ArrayIndexOutOfRange', ('Array index out of range', 
-        f_and(exception_category, array_index_out_of_range)
-    )),
-    ('Exception-InvalidRangeCreation', ('Invalid range creation', 
-        f_and(exception_category, invalid_range_creation)
-    )),
-    ('Exception-RemoveLastFromEmptyCollection', ('Attempted to remove last element from empty collection', 
-        f_and(exception_category, cant_remove_last_elem_from_empty_collection)
-    )),
-    ('Exception-RemoveFirstFromEmptyCollection', ('Attempted to remove first element from empty collection', 
-        f_and(exception_category, cant_remove_first_elem_from_empty_collection)
-    )),
-    ('Exception-DivisionByZeroInRemainder', ('Division by zero when calculating remainder (mod)', 
-        f_and(exception_category, div_by_zero_remainder)
-    )),
-    ('Exception-NegativeArrayIndex', ('Negative array index', 
-        f_and(exception_category, negative_array_index)
-    )),
-    ('Exception-OverflowUnderflowTrap', ('A Int / UInt overflow / underflow occurred. Note: there isnt anything in the error messages that indicates this but I verified for all 9 cases manually.', 
-        f_and(exception_category, over_under_flow)
-    )),
-    # ('Exception-Else', ('Other exceptions', 
-    #     f_and(
-    #         exception_category, 
-    #         f_not(f_or(
-    #             assertion_fail, 
-    #             unwrap_nil, 
-    #             index_out_of_range,
-    #             string_index_out_of_bounds,
-    #             array_index_out_of_range,
-    #             invalid_range_creation, 
-    #             cant_remove_last_elem_from_empty_collection,
-    #             cant_remove_first_elem_from_empty_collection,
-    #             div_by_zero_remainder,
-    #             negative_array_index,
-    #             over_under_flow
-    #         ))
-    #     )
-    # )),
-    ('Timeout', ('Is this runtime or compiler or both?', 
-        timeout_category
-    ))
+    ('dummy', ('example description', dummy_category)),
 ])
 
-
-
-    
 
 def main():
     parser = argparse.ArgumentParser(description='Categorize errors')
@@ -150,9 +32,6 @@ def main():
     parser.add_argument('--csv-out', type=str, help='where to write the CSV', default='error_examples/swift_codes.csv')
     parser.add_argument('--n-examples', type=int, help='How many examples for each error code', default=None),
     parser.add_argument('--no-shuffle-examples', action='store_false', help='Disabling shuffling of examples', dest='shuffle_examples', default=True)
-    parser.add_argument('--allow-fallthrough', action='store_true', help='Should fallthrough be allowed?')
-    parser.add_argument('--allow-multimatch', action='store_true', help='Should multimatch be allowed?')
-    
 
     args = parser.parse_args()
     
@@ -184,28 +63,18 @@ def main():
             did_find_cat = False
             for cat_name, (_, cat_fn) in CATEGORY_DEFINITIONS.items():
                 if cat_fn(comp.exit_code, comp.status, comp.stderr, comp.stdout):
-                    if did_find_cat and not args.allow_multimatch:
-                        raise Exception(f'prob = {prob.name}, comp_idx = {comp_idx} is in multiple categories.')
-                    elif did_find_cat:
-                        print(f'Warning: prob = {prob.name}, comp_idx = {comp_idx} is in multiple categories.')
+                    assert not did_find_cat
                     did_find_cat = True
                     category_results[cat_name].append((prob.name, comp_idx))
-            
-            if not did_find_cat:
-                if args.allow_fallthrough:
-                    if 'fallthrough' not in category_results:
-                        category_results['fallthrough'] = []
-                    category_results['fallthrough'].append((prob.name, comp_idx))
-                else:
-                    raise Exception(f'No category matched for prob = {prob.name}, comp_idx = {comp_idx}.')
-                    
+            assert did_find_cat
+    
     with csv_out.open('w', newline='') as csv_f:
         csv_writer = csv.writer(csv_f)
         csv_writer.writerow(['code', 'description', 'count', 'example'])
 
         for cat_name, example_keys in category_results.items():
-            desc = 'SHOULD BE EMPTY' if cat_name == 'fallthrough' else CATEGORY_DEFINITIONS[cat_name][0]
-            example_out = examples_out_dir / f"{cat_name}.swift"
+            desc = CATEGORY_DEFINITIONS[cat_name][0]
+            example_out = examples_out_dir / f"{cat_name}.{args.lang}"
             cat_count = len(example_keys)
 
             if args.shuffle_examples:
