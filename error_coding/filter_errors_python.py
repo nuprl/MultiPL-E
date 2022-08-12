4ff717df9e296b8aa9505b786f134c289bf760d6
Abhinav Jangda <abhijangda@gmail.com>
2022-08-12 19:49:39 -0400

    Revert "move classficications"
    This reverts commit 0374fae90ba0a1d1aa4d502f51d56a1dd84aef7b.

--- error_coding/filter_errors_python.py
+++ error_coding/filter_errors_python.py
@@ -10,7 +10,6 @@ import re
 from random import shuffle
 from typing import Callable, Dict, List, Tuple
 import yaml
-import classifications
 try:
     from yaml import CLoader as YAML_Loader
 except ImportError:
