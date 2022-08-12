a1f3916711ef792e0dbd2838fabc6fd14440c89a
Arjun Guha <a.guha@northeastern.edu>
2022-08-12 08:46:34 -0400

    Premium evaluator for TS

--- src/eval_ts.py
+++ src/eval_ts.py
@@ -1,62 +1,33 @@
-# Authored by Carolyn Anderson
-import os
-import subprocess
 from pathlib import Path
+from safe_subprocess import run
+
 
 def eval_script(path: Path):
-    try: 
-        # Assumes exit-code 0 is all okay
-        # Run racket on the file, capturing stderr
-        compiled = subprocess.run(["tsc", str(path)], capture_output=True, timeout=15)
-        if compiled.returncode == 0:
-            pass
-        else:
-            return {
-            "status": "CompileError",
-            "exit_code": compiled.returncode,
-            "stdout": compiled.stdout,
-            "stderr": compiled.stderr,
-                }
-        output = subprocess.run(["node", str(path).replace('.ts','.js')], cwd=path.parent, capture_output=True, timeout=15)
-        
-        if output.returncode == 0:
-            status = "OK"
-        else:
-            outmessage = str(output)
-            if 'ERR_ASSERTION' in outmessage:
-                status = "AssertionError"
-            elif 'SyntaxError' in outmessage:
-                status = "SyntaxError"
-            elif 'ReferenceError' in outmessage:
-                status = "ReferenceError"
-            else:
-                status = "Exception"
-        returncode = output.returncode
-    except subprocess.TimeoutExpired as exc:
+    r = run(["tsc", "--target", "esnext", str(path)], timeout_seconds=15)
+    if r.exit_code != 0:
+        return {
+            "status": "SyntaxError",
+            "exit_code": r.exit_code,
+            "stdout": r.stdout,
+            "stderr": r.stderr,
+        }
+
+    r = run(["node", str(path).replace(".ts", ".js")], timeout_seconds=15)
+    if r.timeout:
         status = "Timeout"
-        output = exc
-        returncode = -1
-    except subprocess.CalledProcessError as exc:
+    elif r.exit_code == 0:
+        status = "OK"
+    elif "ERR_ASSERTION" in r.stderr:
+        status = "AssertionError"
+    elif "SyntaxError" in r.stderr:
+        status = "SyntaxError"
+    elif "ReferenceError" in r.stderr:
+        status = "ReferenceError"
+    else:
         status = "Exception"
-        returncode = exc.returncode
-        output = exc
     return {
-            "status": status,
-            "exit_code": returncode,
-            "stdout": output.stdout,
-            "stderr": output.stderr,
-                }
-
-
-
-def main():
-    directory = Path(Path(__file__).parent, "..", "datasets", "ts-keep-code_davinci_001_temp_0.2").resolve()
-
-    for filename in os.listdir(directory):
-        if ".ts" in filename:
-            r = eval_script(Path.joinpath(directory,filename))
-            filename = filename.split(".")[0]
-            print(f"TypeScript,{filename},{r['status']}")
-
-if __name__ == "__main__":
-    main()
+        "status": status,
+        "exit_code": r.exit_code,
+        "stdout": r.stdout,
+        "stderr": r.stderr,
+    }
