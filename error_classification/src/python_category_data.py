35b590d0e01fcef9b19d9d46630fff8f3e83e2d7
Abhinav Jangda <abhijangda@gmail.com>
2022-08-12 23:56:13 -0400

    New directory structure for error classification

---
+++ error_classification/src/python_category_data.py
@@ -0,0 +1,19 @@
+import csv
+
+LANG_NAME = "Python"
+
+CSV_DICT = {}
+with open('python_codes.csv', 'r') as csv_f:
+    csv_reader = csv.reader(csv_f)
+    for r in csv_reader:
+        CSV_DICT[r[0]] = r
+    del CSV_DICT['code'] # kill the header row
+
+def get_description(code: str) -> str:
+    return CSV_DICT[code][1]
+
+def get_total_failures() -> int:
+    return sum([int(CSV_DICT[l][2]) for l in CSV_DICT.keys()])
+
+def get_code_count(code: str) -> int:
+    return int(CSV_DICT[code][2])
