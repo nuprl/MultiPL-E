0ae357ea9150b35ba813d7455de4dde37ef71f1e
Arjun Guha <a.guha@northeastern.edu>
2022-11-16 06:35:00 -0500

    Calculate pass-k in Python
    This beats repeating the list of experiments in Rust and Python, which
    we did before.
    
    Now that all data is compressed, Python runs fast enough.

--- cluster/__main__.py
+++ cluster/__main__.py
@@ -8,6 +8,7 @@ import tqdm
 import math
 from dataset_builder.libexperiments import all_experiments, Experiment
 from typing import Optional, List
+import numpy as np
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -72,6 +73,52 @@ def build_job_file(args):
             f.write("\n")
 
 
+def estimate_passk(n: int, c: int, k: int) -> float:
+    """
+    Calculates 1 - comb(n - c, k) / comb(n, k).
+    """
+    if n - c < k:
+        return 1.0
+    return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+
+def single_problem_pass_k_for_experiment(exp: Experiment) -> List[str]:
+    results = [ ]
+    dataset = exp.dataset
+    lang = exp.lang
+    model = exp.model
+    variation = exp.variation
+    for result_path in exp.path().glob("*.results.json.gz"):
+        results_json = gunzip_json(result_path)
+        if results_json is None:
+            eprint(f"Ignoring {result_path}")
+            continue
+        problem = result_path.name[:-len(".results.json.gz")]
+        n = len(results_json["results"])
+        c = sum((1 for r in results_json["results"] if r["status"] == "OK" and r["exit_code"] == 0))
+        if exp.temp == "0.2":
+            pass1 = estimate_passk(n, c, 1)
+            results.append(f"{dataset},{lang},{model},{variation},{problem},{pass1},{n},NA,NA,NA")
+        elif exp.temp == "0.8":
+            pass10 = estimate_passk(n, c, 10)
+            pass100 = estimate_passk(n, c, 100)
+            results.append(f"{dataset},{lang},{model},{variation},{problem},NA,NA,{pass10},{n},{pass100}")
+        else:
+            raise ValueError(f"Unknown temperature {exp.temp}")
+    return results
+
+
+def single_problem_pass_k(args):
+    output: Path = args.output
+    with output.open("wt") as f:
+        f.write("dataset,lang,problem,model,variation,pass@1,n(t=0.2),pass@10,n(t=0.8),pass@100\n")
+        exps = list(all_experiments())
+        with ProcessPoolExecutor() as executor:
+            for results in tqdm.tqdm(executor.map(single_problem_pass_k_for_experiment, exps), total=len(exps)):
+                for line in results:
+                    f.write(line)
+                    f.write("\n")
+
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -82,6 +129,10 @@ def main():
     build_job_file_parser.add_argument("--max-jobs", type=int, default=1000)
     build_job_file_parser.add_argument("--min-job-size", type=int, default=50)
     build_job_file_parser.set_defaults(func=lambda: build_job_file(args))
+
+    single_problem_pass_k_parser = subparsers.add_parser("single-problem-pass-k")
+    single_problem_pass_k_parser.add_argument("--output", type=Path, required=True)
+    single_problem_pass_k_parser.set_defaults(func=lambda: single_problem_pass_k(args))
 
     args = parser.parse_args()
     if not hasattr(args, "func"):
