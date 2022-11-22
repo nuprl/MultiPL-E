45746da2c59d537a23f9dda7d71d543ff7cacbf4
Yangtian Zi <zi.ya@northeastern.edu>
2022-11-22 02:44:53 +0000

    added .gz support for pass-k calculation

--- src/single_experiment_pass_k.py
+++ src/single_experiment_pass_k.py
@@ -1,7 +1,9 @@
 import numpy as np
 from pathlib import Path
 import json
+import itertools
 import argparse
+import gzip
 
 def estimator(n: int, c: int, k: int) -> float:
     """
@@ -11,8 +13,12 @@ def estimator(n: int, c: int, k: int) -> float:
         return 1.0
     return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
 
+
+def open_json(fpath: Path, mode: str):
+        return  gzip.open(fpath, mode + "t") if fpath.suffix == ".gz" else open(fpath, mode)
+
 def for_file(path):
-    with open(path) as f:
+    with open_json(path, "r") as f:
         data = json.load(f)
     n = len(data["results"])
     c = len([True for r in data["results"] if r["status"] == "OK" and r["exit_code"] == 0])
@@ -26,7 +32,7 @@ def main():
         print(d)
         print("NOTE: Use pass@1 for temperature 0.2 and pass@10 or pass@100 for temperature 0.8")
         print("[pass@1 pass@10 pass@100]")
-        print(np.array([ for_file(p) for p in Path(d).glob("*.results.json") ]).mean(axis=0))
+        print(np.array([ for_file(p) for p in itertools.chain(Path(d).glob("*.results.json"), Path(d).glob("*.results.json.gz")) ]).mean(axis=0))
 
 if __name__ == "__main__":
     main()
