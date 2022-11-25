03560fb983e4eba311b97aef7940a7229c8401d5
Arjun Guha <a.guha@northeastern.edu>
2022-11-25 11:14:30 -0500

    How foolish we were to think that compilers always halt.

--- evaluation/src/eval_scala.py
+++ evaluation/src/eval_scala.py
@@ -1,11 +1,5 @@
-import os
-import subprocess
 from pathlib import Path
-
-import os
-import subprocess
 import tempfile
-from pathlib import Path
 from safe_subprocess import run
 
 LANG_NAME = "Scala"
@@ -13,21 +7,21 @@ LANG_EXT = ".scala"
 
 def eval_script(path: Path):
     with tempfile.TemporaryDirectory() as outdir:
-        #Each Scala file contains the class with same name `JAVA_CLASS_NAME`
-        #Hence, scalac will same JAVA_CLASS_NAME.class file for each problem
-        #Write class for each problem to a different temp dir
-        build = subprocess.run(["scalac", "-d", outdir, path], capture_output=True, encoding="utf-8")
-        exec_name = "Problem" #JAVA_CLASS_NAME
-        if build.returncode != 0:
+        # Each Scala file contains the class with same name `JAVA_CLASS_NAME`
+        # Hence, scalac will same JAVA_CLASS_NAME.class file for each problem
+        # Write class for each problem to a different temp dir
+        build = run(["scalac", "-d", outdir, path], timeout_seconds=45)
+        if build.exit_code != 0:
             # Well, it's a compile error. May be a type error or
             # something. But, why break the set convention
             return {
                 "status": "SyntaxError",
-                "exit_code": build.returncode,
+                "exit_code": build.exit_code,
                 "stdout": build.stdout,
                 "stderr": build.stderr,
             }
-        r = run(["scala", "-cp", f"{outdir}", exec_name])
+        # "Problem" is the name of the class we emit.
+        r = run(["scala", "-cp", f"{outdir}", "Problem"])
         if r.timeout:
             status = "Timeout"
         elif r.exit_code == 0 and r.stderr == "":
@@ -41,6 +35,3 @@ def eval_script(path: Path):
         "stdout": r.stdout,
         "stderr": r.stderr,
     }
-
-if __name__ == "__main__":
-    main(eval_script, LANG_NAME, LANG_EXT)
--- evaluation/src/eval_swift.py
+++ evaluation/src/eval_swift.py
@@ -1,51 +1,30 @@
-import argparse
-from sys import exit
 import subprocess
 from pathlib import Path
-from generic_eval import main
 import os
+from safe_subprocess import run
 
 def eval_script(path: Path):
     basename = ".".join(str(path).split(".")[:-1])
-    try:
-        build = subprocess.run(["swiftc", path, "-o", basename], capture_output=True)
-    except subprocess.TimeoutExpired as exc:
-        return {
-            "status": "Timeout",
-            "exit_code": -1,
-            "stdout": "Compiler timeout",
-            "stderr": "Compiler timeout",
-        }
-    status = None
-    returncode = -1
-    output = None
-    if build.returncode != 0:
+    r = run(["swiftc", path, "-o", basename], timeout_seconds=45)
+    if r.timeout:
+        status = "Timeout"
+    elif r.exit_code != 0:
         # Well, it's a compile error. May be a type error or
         # something. But, why break the set convention
         status = "SyntaxError"
-        returncode = build.returncode
-        output = build
     else:
-        try:
-            # Assumes exit-code 0 is all okay
-            output = subprocess.run([basename], capture_output=True, timeout=5)
-            returncode = output.returncode
-            if output.returncode == 0:
-                status = "OK"
-            else:
-                # Well, it's a panic
-                status = "Exception"
-        except subprocess.TimeoutExpired as exc:
+        r = run([basename], timeout_seconds=5)
+        if r.timeout:
             status = "Timeout"
-            output = exc
+        elif r.exit_code != 0:
+            # Well, it's a panic
+            status = "Exception"
+        else:
+            status = "OK"
         os.remove(basename)
     return {
         "status": status,
-        "exit_code": returncode,
-        "stdout": output.stdout,
-        "stderr": output.stderr,
+        "exit_code": r.exit_code,
+        "stdout": r.stdout,
+        "stderr": r.stderr,
     }
-
-
-if __name__ == "__main__":
-    main(eval_script, 'Swift', '.swift')
