f96c035c6899682f22670a56f50e30a6225a2adb
Arjun Guha <a.guha@northeastern.edu>
2022-12-22 05:23:16 -0500

    FIM for many BigCode models

--- inference/bigcode_fim.py
+++ inference/bigcode_fim.py
@@ -5,3 +5,4 @@ revision = "60a5975"
 model = Model("bigcode/christmas-models", revision=revision)
 completions = model.completions
 name = "bigcode_1B_60a5975_fim"
+fill_in_the_middle = model.fill_in_the_middle
--- inference/bigcode_stars.py
+++ inference/bigcode_stars.py
@@ -5,3 +5,4 @@ revision = "d33d4e0"
 model = Model("bigcode/christmas-models", revision=revision)
 completions = model.completions
 name = f"bigcode_1B_{revision}_stars"
+fill_in_the_middle = model.fill_in_the_middle
