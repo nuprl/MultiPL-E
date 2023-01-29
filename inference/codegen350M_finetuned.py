b565196cdeb62e2e50cb612c40b2ebed180e72cc
Claire Schlesinger <esslushy@gmail.com>
2023-01-29 12:27:15 -0500

    added local inference for local codegen model

---
+++ inference/codegen350M_finetuned.py
@@ -0,0 +1,10 @@
+from local_huggingface_model import LocalHuggingfaceModel
+
+_model = LocalHuggingfaceModel(
+    "inference/local_models/codegen_fs", model_kwargs={ 'trust_remote_code': True },
+    set_pad_token_id_plus_2=True
+)
+
+name = "codegen_finetuned"
+
+completions = _model.completions
