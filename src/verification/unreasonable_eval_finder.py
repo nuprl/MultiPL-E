f27c59244798115ec7d5a9aabf044ed5f9f25676
Claire Schlesinger <esslushy@gmail.com>
2023-02-03 22:22:03 -0500

    working on building some scripts to understand why such a high pass@k

---
+++ src/verification/unreasonable_eval_finder.py
@@ -0,0 +1,13 @@
+# This file is used to find eval results that don't make sense.
+# e.g status ok but stderr has output, or lots of them with syntax errors.
+
+import argparse
+import json
+
+def main():
+    pass
+
+
+
+if __name__ == "main":
+    main()
