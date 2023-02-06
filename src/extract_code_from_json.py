b20859ca3fec57db42135d59cc8181453c5e9d6b
Claire Schlesinger <esslushy@gmail.com>
2023-02-06 12:13:18 -0500

    modified f# extractor to be more general

--- src/extract_code_from_json.py
+++ src/extract_code_from_json.py
@@ -7,9 +7,11 @@ def main():
 
     args = argparse.ArgumentParser()
 
-    args.add_argument("--input", type=Path, required=True)
-    # list of files to extract
-    args.add_argument("--files", type=int, nargs="+", required=True)
+    args.add_argument("--input", help="The json file storing the evaluated completions.", type=Path, required=True)
+    # list of programs to extract
+    args.add_argument("--programs", help="The numbers associated with each program in the results.", type=int, nargs="+", required=True)
+
+    args.add_argument("--lang", help="The file extension to save the program as.", type=str, required=True)
 
     args.add_argument("--output-dir", type=Path, default="output/")
 
@@ -21,8 +23,8 @@ def main():
         result_json = json.load(f) 
         results = result_json["results"]
 
-        for i in args.files:
-            with open(args.output_dir / f"{i}-{results[i]['status']}.fsx", "w") as f:
+        for i in args.programs:
+            with open(args.output_dir / f"{i}-{results[i]['status']}.{args.lang}", "w") as f:
                 f.write(results[i]["program"])
 
 if __name__ == "__main__":
