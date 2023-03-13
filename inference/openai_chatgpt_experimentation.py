560eb26bda16bd0a429999c83eacc581324784c4
Claire Schlesinger <esslushy@gmail.com>
2023-03-13 19:42:15 -0400

    added experimentation file

--- inference/openai_chatgpt_experimentation.py
+++ inference/openai_chatgpt_experimentation.py
@@ -3,8 +3,29 @@ from openai.error import RateLimitError, APIConnectionError, Timeout
 import random
 import yaml
 from time import sleep
+import os
+import json
 
 name = "chatgpt"
+
+system_role = 0
+prompt_lead = 0
+
+expt_out = f"experiment-{system_role}-{prompt_lead}.json"
+
+system_roles = [
+    "You are a programmer whose job it is to finish the functions provided by the user.", 
+    "You're job is to write the functions asked of you by the user.",
+    "you are a large language model whose job it is to complete code prompts, \
+        determining the language to be written in based on the apparent language of the prompt.",
+    "You are a language model whose job it is to produce the full function that fulfills the prompt."
+]
+prompt_leads = [
+    "{}",
+    "Please finish this function: {}" ,
+    "Write the function for me that fulfills the prompt: {}",
+    "Please write the entire function that matches {}"
+]
 
 # Set up the configuration file
 with open("inference/chatgpt/config.yaml") as f:
@@ -18,8 +39,8 @@ def completions(prompt: str, max_tokens: int, temperature: float, n: int, top_p,
         model="gpt-3.5-turbo",
         messages=[
             # This tells the chatbot what role it is fulfilling.
-            {"role": "system", "content":  "The user will give you a function prompt. Your job is to write the function and explain how it works."},
-            {"role": "user", "content": "Please write a function that could fulfill this prompt " + prompt}
+            {"role": "system", "content":  system_roles[system_role]},
+            {"role": "user", "content": prompt_leads[prompt_lead].format(prompt)}
         ],
         temperature=temperature,
         top_p=top_p,
@@ -31,7 +52,23 @@ def completions(prompt: str, max_tokens: int, temperature: float, n: int, top_p,
     if config["debug"]:
         print(completion_messages)
     # pull out the code body
-    return get_code_body(completion_messages)
+    processed_response = get_code_body(completion_messages)
+    # Save experiment data
+    raw_data = {
+        "system_prompt" : system_roles[system_role],
+        "user_prompt" : prompt_leads[prompt_lead].format(prompt),
+        "raw_response" : completion_messages,
+        "processed_response" : processed_response
+    }
+    if os.path.exists(expt_out):
+        with open(expt_out, "w+") as f:
+            so_far = json.load(f)
+    else:
+        so_far = []
+    so_far.append(raw_data)
+    with open(expt_out, "w+") as f:
+        json.dump(so_far, f)    
+    return processed_response
 
 def complete_or_fail_after_n_tries(func, n):
     if n == 0:
