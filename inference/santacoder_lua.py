0ea7bc10c50ae8e7331fac7f6543f8360f0a2d28
Arjun Guha <a.guha@northeastern.edu>
2023-04-06 17:23:57 -0400

    A new Lua model

--- inference/santacoder_lua.py
+++ inference/santacoder_lua.py
@@ -1,8 +1,8 @@
 from .santacoder_base import Model
 
 
-revision = "e57b3c39fd29e36ba86970e49618448f5d3d5529"
-model = Model("arjunguha/santacoder-lua", revision=revision)
+revision = "d3dab9c"
+model = Model("arjunguha/santacoder-lua-nofim", revision=revision)
 completions = model.completions
-name = f"santacoder_lua_{revision}"
+name = f"santacoder_lua_nofim_{revision}"
 fill_in_the_middle = model.fill_in_the_middle
