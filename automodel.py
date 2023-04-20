8aa554a0b743d528971095fb971d30f94bff1b0b
Arjun Guha <a.guha@northeastern.edu>
2023-04-20 18:20:21 -0400

    Generic automodel script

---
+++ automodel.py
@@ -0,0 +1,77 @@
+"""
+This script produces completions for roughly any AutoModelForCausalLM.
+"""
+from multipl_e.completions import make_main, stop_at_stop_token, partial_arg_parser
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class Model:
+    def __init__(self, name, revision):
+        self.model = AutoModelForCausalLM.from_pretrained(name, revision=revision)
+        self.model = self.model.half().cuda()`
+        # self.model = self.model.cuda()
+        self.tokenizer = AutoTokenizer.from_pretrained(name, revision=revision, padding_side="left")
+        self.tokenizer.pad_token = "<|endoftext|>"
+        
+    def completion_tensors(
+        self,
+        prompt: str,
+        max_length: int,
+        temperature: float,
+        n: int,
+        top_p: float,
+    ):
+        """
+        Produces n samples.
+        """
+        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = input_ids.cuda()
+        max_length = max_length + input_ids.flatten().size(0)
+        attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device="cuda")
+        with torch.no_grad():
+            output = self.model.generate(
+                input_ids=input_ids,
+                do_sample=True,
+                top_p=top_p,
+                temperature=temperature,
+                num_return_sequences=n,
+                max_length=max_length,
+                attention_mask=attention_mask,
+                pad_token_id=self.tokenizer.pad_token_id
+            )
+        return output
+
+    def decode_single_output(self, output_tensor, prompt):
+        detok_hypo_str = self.tokenizer.decode(
+            output_tensor, clean_up_tokenization_spaces=False
+        )
+        # Skip the prompt (which may even have stop_tokens)
+        return detok_hypo_str[len(prompt) :]
+
+    def completions(
+        self, prompt: str, max_tokens: int, temperature: float, n: int, top_p, stop
+    ):
+        prompt = prompt.strip()
+        output_tensors = self.completion_tensors(
+            prompt,
+            max_tokens,
+            temperature,
+            n,
+            top_p,
+        )
+        return [
+            stop_at_stop_token(self.decode_single_output(output_tensor, prompt), stop)
+            for output_tensor in output_tensors
+        ]
+
+def main():
+    args = partial_arg_parser()
+    args.add_argument("--name", type=str, required=True)
+    args.add_argument("--revision", type=str)
+    args = args.parse_args()
+    model = Model(args.name, args.revision)
+    name = args.name.replace("/", "_").replace("-", "_")
+    make_main(args, name, model.completions)
+
+if __name__ == "__main__":
+    main()
