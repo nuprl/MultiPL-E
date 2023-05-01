637291d7a38388f81cfcd69e2fc545cefc386605
Arjun Guha <a.guha@northeastern.edu>
2023-05-01 21:41:31 +0000

    FIM inference

--- fim_inference.py
+++ fim_inference.py
@@ -1,23 +1,25 @@
+# I've hacked this to hardcode BigCode15B.
 import json
 from pathlib import Path
-import importlib
 import argparse
 from more_itertools import chunked
 from tqdm import tqdm
+import bigcode15b
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, required=True, help="Module name of the model to use")
+    # parser.add_argument("--model", type=str, required=True, help="Module name of the model to use")
     parser.add_argument("--batch-size", type=int, required=True, help="Batch size to use")
     parser.add_argument("--output-dir", type=Path, default=Path("."), help="Output directory for results")
 
     args = parser.parse_args()
 
-    model = importlib.import_module(args.model)
+    model = bigcode15b.Model(bigcode15b.CHECKPOINT_TO_REVISION["1000m"])
+    name = "bigcode15b"
 
     # Load existing results if any
-    result_path = args.output_dir / f"fim-results-{model.name}.jsonl"
+    result_path = args.output_dir / f"fim-results-{name}.jsonl"
     if result_path.exists():
         with result_path.open("rt") as f:
             results = [ json.loads(line) for line in f ]
@@ -31,13 +33,14 @@ def main():
 
     problems = list(chunked(problems, args.batch_size))
 
+
     with result_path.open("at") as f:
         for batch in tqdm(problems, unit="Batch", desc="FIM inference", total=len(problems)):
             pairs = [ (p["prompt"], p["suffix"]) for p in batch ]
             batch_results = model.fill_in_the_middle(pairs, 25, 0.2)
             for problem, result in zip(batch, batch_results):
                 problem["result"] = result
-                problem["model"] = model.name
+                problem["model"] = name
                 problem["exact_match"] = result.strip() == problem["canonical_solution"].strip()
             for result in batch:
                 f.write(json.dumps(result))
