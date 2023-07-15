436a48a0f75d119bf1481177c555d0d637f249ad
cassanof <federico.cassano@federico.codes>
2023-07-14 18:16:14 -0700

    annoying

--- dataset_builder/generic_translator.py
+++ dataset_builder/generic_translator.py
@@ -179,7 +179,12 @@ def translate_tests(translator, py_tests: str, entry_point: str, filename: str) 
         assert(LHS == RHS)
         ...
     """
-    tests_ast = ast.parse(py_tests, filename)
+    try:
+        tests_ast = ast.parse(py_tests, filename)
+    except Exception as e:
+        print(f"Exception parsing tests for {filename}: {e}")
+        traceback.print_exception(e)
+        return None
     test_cases = translator.test_suite_prefix_lines(entry_point)
     match tests_ast:
         case ast.Module(body=[ast.FunctionDef(body=body)]):
