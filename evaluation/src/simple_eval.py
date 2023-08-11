a008658d6f804fe7591530d997fc1ed86a83e44e
GouwarPower <jgouwar@gmail.com>
2023-08-11 08:06:47 -0400

    Simple eval.

--- evaluation/src/simple_eval.py
+++ evaluation/src/simple_eval.py
@@ -4,10 +4,16 @@ from containerized_eval import eval_string_script
 
 def cli():
     args = argparse.ArgumentParser()
-    args.add_argument("--file", required=True, type=str, help="The file to evaluate")
     args.add_argument("--lang", required=True, type=str, help="The language of the file")
     return args
 
 if __name__ == "__main__":
     args = cli().parse_args() 
-    print(json.dumps(eval_string_script(args.file, args.lang)))
+    prog_str = ""
+    while True:
+        try:
+            line = input()
+            prog_str += line + "\n"
+        except EOFError:
+            break
+    print(json.dumps(eval_string_script(args.lang, prog_str)), end="")
