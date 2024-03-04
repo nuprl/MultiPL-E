3608f4057764669c36d3c7f973fc739a14a87f79
Alex Miller <alex.miller@cognitect.com>
2024-03-04 08:35:42 -0600

    fix eval settings

--- evaluation/src/eval_clj.py
+++ evaluation/src/eval_clj.py
@@ -8,7 +8,7 @@ from libeval import run_without_exn
 
 
 def eval_script(path: Path):
-    result = run(["clojure", "-M", "-Dclojure.main.report=stderr", str(path)])
+    result = run(["clojure", "-J-Dclojure.main.report=stderr", "-M", str(path)])
 
     # will produce exit code 0 even if tests fail.
     if len(result.stderr) > 0 or result.exit_code != 0:
