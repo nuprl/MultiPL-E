1d2b0bb113d3386e349dd3a0214e9d16ca422e8c
Devon Carew <devoncarew@gmail.com>
2024-08-19 18:22:35 -0700

    fix syntax error

--- dataset_builder/humaneval_to_dart.py
+++ dataset_builder/humaneval_to_dart.py
@@ -82,11 +82,7 @@ class Translator:
 
     def translate_prompt(self, name: str, args: List[ast.arg], returns, description: str) -> str:
         global needs_hashmap
-        description = (
-            "// " + re.sub(DOCSTRING_LINESTART_RE + "\n",
-            "// ",
-            description.strip()) + "\n",
-        )
+        description = "//" + re.sub(DOCSTRING_LINESTART_RE, "\n// ", description.strip()) + "\n"
         # Store this for later coercions on tests
         needs_hashmap = False
         self.type = [[arg.annotation for arg in args], returns]
