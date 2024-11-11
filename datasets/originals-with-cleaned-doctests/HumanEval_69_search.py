6cd6ae62389652451625b9075ab5520d35d84c20
Rowan Walshe <walshe@adacore.com>
2024-11-11 14:18:54 +0000

    Fix doc tests and remove trailing whitespaces from modified files

--- datasets/originals-with-cleaned-doctests/HumanEval_69_search.py
+++ datasets/originals-with-cleaned-doctests/HumanEval_69_search.py
@@ -2,14 +2,14 @@ from typing import List
 
 def search(lst: List[int]) -> int:
     '''
-    You are given a non-empty list of positive integers. Return the greatest integer that is greater than 
-    zero, and has a frequency greater than or equal to the value of the integer itself. 
+    You are given a non-empty list of positive integers. Return the greatest integer that is greater than
+    zero, and has a frequency greater than or equal to the value of the integer itself.
     The frequency of an integer is the number of times it appears in the list.
     If no such a value exist, return -1.
     Examples:
     >>> search([4, 1, 2, 2, 3, 1])
     2
-    >>>  search([1, 2, 2, 3, 3, 3, 4, 4, 4])
+    >>> search([1, 2, 2, 3, 3, 3, 4, 4, 4])
     3
     >>> search([5, 5, 4, 4, 4])
     -1
@@ -23,7 +23,7 @@ def search(lst: List[int]) -> int:
     for i in range(1, len(frq)):
         if frq[i] >= i:
             ans = i
-    
+
     return ans
 
 ### Unit tests below ###
