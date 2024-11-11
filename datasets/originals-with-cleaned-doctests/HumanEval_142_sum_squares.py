3c7ecb7abe7a922672bbf10854ceefd7cf79cbbc
Rowan Walshe <walshe@adacore.com>
2024-11-11 14:18:54 +0000

    Fix doc tests and remove trailing whitespaces from modified files

--- datasets/originals-with-cleaned-doctests/HumanEval_142_sum_squares.py
+++ datasets/originals-with-cleaned-doctests/HumanEval_142_sum_squares.py
@@ -2,17 +2,17 @@ from typing import List
 
 def sum_squares(lst: List[int]) -> int:
     """"
-    This function will take a list of integers. For all entries in the list, the function shall square the integer entry if its index is a 
-    multiple of 3 and will cube the integer entry if its index is a multiple of 4 and not a multiple of 3. The function will not 
-    change the entries in the list whose indexes are not a multiple of 3 or 4. The function shall then return the sum of all entries. 
-    
+    This function will take a list of integers. For all entries in the list, the function shall square the integer entry if its index is a
+    multiple of 3 and will cube the integer entry if its index is a multiple of 4 and not a multiple of 3. The function will not
+    change the entries in the list whose indexes are not a multiple of 3 or 4. The function shall then return the sum of all entries.
+
     Examples:
-    >>> lst 
-    [1,2,3] 
-    >>> lst 
-    []  
-    >>> lst 
-    [-1,-5,2,-1,-5]
+    >>> sum_squares([1,2,3])
+    6
+    >>> sum_squares([])
+    0
+    >>> sum_squares([-1,-5,2,-1,-5])
+    -126
     """
     ### Canonical solution below ###
     result =[]
@@ -29,7 +29,7 @@ def sum_squares(lst: List[int]) -> int:
 def check(candidate):
 
     # Check some simple cases
-    
+
     assert candidate([1,2,3]) == 6
     assert candidate([1,4,9]) == 14
     assert candidate([]) == 0
@@ -41,8 +41,8 @@ def check(candidate):
     assert candidate([-1,0,0,0,0,0,0,0,-1]) == 0
     assert candidate([-16, -9, -2, 36, 36, 26, -20, 25, -40, 20, -4, 12, -26, 35, 37]) == -14196
     assert candidate([-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10]) == -1448
-    
-    
+
+
     # Don't remove this line:
 def test_check():
     check(sum_squares)
