2c075a50e648c7c1301249f48a33a52b7978319d
Rudolf Adamkovič <rudolf@adamkovic.org>
2026-03-24 18:22:12 +0100

    Add support for the Tcl programming language

--- evaluation/src/containerized_eval.py
+++ evaluation/src/containerized_eval.py
@@ -29,6 +29,7 @@ import eval_clj
 import eval_v
 import eval_lean
 import eval_dart
+import eval_tcl
 import tempfile
 
 
@@ -65,6 +66,7 @@ EVALUATORS = {
     "coq": (eval_v.eval_script, ".v"),
     "lean": (eval_lean.eval_script, ".lean"),
     "dart": (eval_dart.eval_script, ".dart"),
+    "tcl": (eval_tcl.eval_script, ".tcl"),
 }
 
 def eval_string_script(language, program):
